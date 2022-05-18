75f75a9f5230721f0818e5338d218df7e5bd9bb9
sou <ocelot2828@gmail.com>
2022-05-18 21:46:39 +0900

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+  webpack: (config, { isServer }) => {
+    config.experiments = {
+      asyncWebAssembly: true,
+    };
+    config.output.webassemblyModuleFilename =
+      (isServer ? "../" : "") + "static/wasm/webassembly.wasm";
+    return config;
+  },
+};
