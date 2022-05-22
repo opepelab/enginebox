ec193058b0b228f6d7af45941470e58741cd7fc5
sou <ocelot2828@gmail.com>
2022-05-22 11:14:05 +0900

    first

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,43 @@
 import { Text, Box } from "@chakra-ui/react";
+import { GetStaticProps } from "next";
 
-export default function Home() {
+type Map = {
+  data: {
+    map: StringConstructor;
+  };
+};
+
+const Index: React.FC<Map> = ({ data }) => {
   return (
-    <div>
-      <Text>tes</Text>
-    </div>
+    <main>
+      <div>
+        <Text>tes</Text>
+        {data.map((data: any) => (
+          <dl key={data.id}>
+            {/* <dt className="dateST">
+              <Date dateString={data.date} />
+            </dt> */}
+            {/* <Link href={`/docs/url/${data.slug}`}> */}
+            <a>
+              <div>{data.title}</div>
+            </a>
+            {/* </Link> */}
+          </dl>
+        ))}
+      </div>
+    </main>
   );
-}
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await fetch("http://localhost:8080/getjson/");
+  const data = await res.json();
+
+  return {
+    props: {
+      blog: data.items,
+    },
+  };
+};
+
+export default Index;
