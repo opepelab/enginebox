ed387e160b0f3875290003c1ac2b140c7d4cffb3
sou <ocelot2828@gmail.com>
2022-06-15 02:18:42 +0900

    save

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -10,19 +10,17 @@ export default (req: NextApiRequest, res: NextApiResponse): void => {
           ja: '変数を学ぼう',
           en: 'Variable',
         },
-        body: 'test',
+        body: 'x = ~',
       },
       {
         id: 2,
-        title: '関数を学ぼう',
-        body: `関数は最初にconstで代入する関数を宣言する。
-            const Index = ({引数}) => {
-                return (
-            戻り値
-        )
-        }
+        title: {
+          ja: '関数を学ぼう',
+          en: 'Learn functions',
+        },
+        body: `関数は最初にconstで代入する関数を宣言する。<br/>
+        <code>const Index = ({引数}) => { return (戻り値)}</code> \n
         アロー関数だとこんな感じだ。
-
         関数の動きの基本は。
         引数を受けとって戻り値に返す。
         これだけだ。
@@ -35,10 +33,8 @@ export default (req: NextApiRequest, res: NextApiResponse): void => {
             戻り値	
             )
         }
-
         こんな感じだ。
         引数っていうのはこれから先ずっと出てくる概念だから重要だぞ。
-
         今回紹介した文法はJavaScriptとTypeScriptの文法になる。`,
       },
     ],
