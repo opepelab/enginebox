a3f3f5841ebe20e4b48d234f12bd6c3c0fb67488
sou <ocelot2828@gmail.com>
2022-06-21 00:03:00 +0900

    Push

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,27 +1,25 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-const items = [
-  {
-    id: 1,
-    slug: 'variable',
-    title: 'するぐいちー',
-    body: {
-      ja: 'てすとー',
-      en: 'testoer',
-    },
-  },
-  {
-    id: 2,
-    slug: 'functions',
-    title: 'するぐつー',
-    body: {
-      ja: 'テスト',
-      en: 'test',
-    },
-  },
-];
-
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   res.statusCode = 200;
-  res.send(items);
+  res.json([
+    {
+      id: 1,
+      slug: 'variable',
+      title: 'するぐいちー',
+      body: {
+        ja: 'てすとー',
+        en: 'testoer',
+      },
+    },
+    {
+      id: 2,
+      slug: 'functions',
+      title: 'するぐつー',
+      body: {
+        ja: 'テスト',
+        en: 'test',
+      },
+    },
+  ]);
 }
