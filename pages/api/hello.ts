b77cce48db6faf490ea1ac5b25476f4eeb332187
sou <ocelot2828@gmail.com>
2022-06-20 16:34:24 +0900

    Push

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,44 +1,42 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default (req: NextApiRequest, res: NextApiResponse): void => {
-  res.status(200).json({
-    items: [
-      {
-        id: 1,
-        slug: 'variable',
-        title: {
-          ja: '変数を学ぼう',
-          en: 'Variable',
-        },
-        body: {
-          ja: 'てすとー',
-          en: 'testoer',
-        },
+  res.status(200).json([
+    {
+      id: 1,
+      slug: 'variable',
+      title: {
+        ja: '変数を学ぼう',
+        en: 'Variable',
       },
-      {
-        id: 2,
-        slug: 'functions',
-        title: {
-          ja: '関数を学ぼう',
-          en: 'Learn functions',
-        },
-        body: {
-          ja: 'テスト',
-          en: 'test',
-        },
+      body: {
+        ja: 'てすとー',
+        en: 'testoer',
       },
-      {
-        id: 3,
-        slug: 'functions',
-        title: {
-          ja: '休憩することを覚えよう',
-          en: 'Variable',
-        },
-        body: {
-          ja: 'てすつ',
-          en: 'testier',
-        },
+    },
+    {
+      id: 2,
+      slug: 'functions',
+      title: {
+        ja: '関数を学ぼう',
+        en: 'Learn functions',
       },
-    ],
-  });
+      body: {
+        ja: 'テスト',
+        en: 'test',
+      },
+    },
+    {
+      id: 3,
+      slug: 'functions',
+      title: {
+        ja: '休憩することを覚えよう',
+        en: 'Variable',
+      },
+      body: {
+        ja: 'てすつ',
+        en: 'testier',
+      },
+    },
+  ]);
 };
