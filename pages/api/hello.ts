92acd01c2492a546cab368d51bb424d19ef0a4ec
sou <ocelot2828@gmail.com>
2022-06-20 23:07:56 +0900

    test

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,37 +1,27 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+const items = [
+  {
+    id: 1,
+    slug: 'variable',
+    title: 'するぐいちー',
+    body: {
+      ja: 'てすとー',
+      en: 'testoer',
+    },
+  },
+  {
+    id: 2,
+    slug: 'functions',
+    title: 'するぐつー',
+    body: {
+      ja: 'テスト',
+      en: 'test',
+    },
+  },
+];
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
   res.statusCode = 200;
-  res.json([
-    {
-      id: 1,
-      slug: 'variable',
-      title: 'するぐいちー',
-      body: {
-        ja: 'てすとー',
-        en: 'testoer',
-      },
-    },
-    {
-      id: 2,
-      slug: 'functions',
-      title: 'するぐつー',
-      body: {
-        ja: 'テスト',
-        en: 'test',
-      },
-    },
-    // {
-    //   id: 2,
-    //   slug: 'functions',
-    //   title: {
-    //     ja: '関数を学ぼう',
-    //     en: 'Learn functions',
-    //   },
-    //   body: {
-    //     ja: 'テスト',
-    //     en: 'test',
-    //   },
-    // },
-  ]);
+  res.json(items);
 }
