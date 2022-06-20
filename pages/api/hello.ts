28ebe41728d7977ee7cf1af9406cdc037ce2c452
sou <ocelot2828@gmail.com>
2022-06-21 01:17:06 +0900

    Push

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,25 +1,29 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.statusCode = 200;
-  res.json([
-    {
-      id: 1,
-      slug: 'variable',
-      title: 'するぐいちー',
-      body: {
-        ja: 'てすとー',
-        en: 'testoer',
+  try {
+    res.statusCode = 200;
+    res.json([
+      {
+        id: 1,
+        slug: 'variable',
+        title: 'するぐいちー',
+        body: {
+          ja: 'てすとー',
+          en: 'testoer',
+        },
       },
-    },
-    {
-      id: 2,
-      slug: 'functions',
-      title: 'するぐつー',
-      body: {
-        ja: 'テスト',
-        en: 'test',
+      {
+        id: 2,
+        slug: 'functions',
+        title: 'するぐつー',
+        body: {
+          ja: 'テスト',
+          en: 'test',
+        },
       },
-    },
-  ]);
+    ]);
+  } catch (err) {
+    res.status(500).json({ error: 'failed to fetch data' });
+  }
 }
