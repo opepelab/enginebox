3c5453834981d91d02377f0481e3cd4e3c059d1a
sou <ocelot2828@gmail.com>
2022-06-21 04:16:14 +0900

    t

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -3,18 +3,20 @@ import { NextApiRequest, NextApiResponse } from 'next';
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     res.status(200);
-    res.send([
-      {
-        id: 1,
-        slug: 'variable',
-        title: 'ヴァリアブル',
-      },
-      {
-        id: 2,
-        slug: 'function',
-        title: 'ファンクション',
-      },
-    ]);
+    res.send({
+      items: [
+        {
+          id: 1,
+          slug: 'variable',
+          title: 'ヴァリアブル',
+        },
+        {
+          id: 2,
+          slug: 'function',
+          title: 'ファンクション',
+        },
+      ],
+    });
   } catch (err) {
     res.status(500).send({ error: 'failed to fetch data' });
   }
