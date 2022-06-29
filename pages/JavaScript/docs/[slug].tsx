57e66a0b43d7edddf45ca5621ba3df6f0443a120
sou <ocelot2828@gmail.com>
2022-06-29 22:25:15 +0900

    save

--- pages/JavaScript/docs/[slug].tsx
+++ pages/JavaScript/docs/[slug].tsx
@@ -10,9 +10,8 @@ type Content = {
 };
 
 type Str = {
-    slug: string;
-}
-
+  slug: string;
+};
 
 const Slug: React.FC<Content> = ({ docs }) => {
   return (
@@ -29,7 +28,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
   const data = await res.json();
 
   const paths: string[] = data.map((item: Str) => ({
-    params: { slug: item.slug },
+    params: { slug: item.slug.toString() },
   }));
 
   return {
@@ -42,14 +41,14 @@ export const getStaticProps: GetStaticProps = async (context) => {
   const slug = context.params?.slug;
   const res = await fetch(`http://127.0.0.1:5000/${slug}/`, {
     headers: {
-      Accept: "application/json; charset=UTF-8",
-      'User-Agent': '*',
+      Accept: 'application/json; charset=UTF-8',
+      'User-Agent': 'MY-UA-STRING',
     },
   });
 
-  // const data =  JSON.stringify(res);
+  // const stringified = JSON.stringify(res);
   // const data = await JSON.parse(stringified);
-  const data = await res.text();
+  const { data } = await res.json();
   // const data = await JSON.parse(JSON.stringify(res))
 
   return {
@@ -62,7 +61,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
 export default Slug;
 
 // {
-//   headers : { 
+//   headers : {
 //     'Content-Type': 'application/json',
 //     'Accept': 'application/json'
 //    }
